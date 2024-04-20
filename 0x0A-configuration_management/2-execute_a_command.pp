3fb021a5987accc8799177e1713e36e7095a8cc1
JuggernautTej <tiwatej@gmail.com>
2024-04-20 02:40:49 +0100

    edited task 2 file again

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
 # This is a manifest that kills a process called killmenow
 exec { 'kill_killmenow_process':
 command     => 'pkill -f killmenow',
-path        => '/bin:/usr/bin',
-refreshonly => true,
-subscribe   => File['/killmenow'],
+path        => '/usr/bin:/usr/sbin:/bin',
 }
