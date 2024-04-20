3634b7fe0c984b700a125a8870d955e7a7a2aa0a
JuggernautTej <tiwatej@gmail.com>
2024-04-20 02:43:03 +0100

    edited task 2 file to comply with puppet lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # This is a manifest that kills a process called killmenow
 exec { 'kill_killmenow_process':
-command     => 'pkill -f killmenow',
-path        => '/usr/bin:/usr/sbin:/bin',
+command => 'pkill -f killmenow',
+path    => '/usr/bin:/usr/sbin:/bin',
 }
