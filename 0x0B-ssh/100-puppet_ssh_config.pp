81bbccef4ef3794af47dfb2fe97e95d1b64b2fec
JuggernautTej <tiwatej@gmail.com>
2024-04-23 17:24:14 +0100

    updated advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,11 +5,11 @@ include stdlib
 file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '  IdentityFile ~/ssh/school',
+  line   => 'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '  PasswordAuthentication no',
+  line   => 'PasswordAuthentication no',
   }
