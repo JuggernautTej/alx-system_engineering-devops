be0aa73446051c3f4e9a300589b5d5f6e7abb727
JuggernautTej <tiwatej@gmail.com>
2024-06-04 13:27:13 +0100

    created new project andd added task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A function that queries the Reddit API and
+returns the number of total subscribers for
+a given subreddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Custom User-Agent'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException as err:
+        return 0
